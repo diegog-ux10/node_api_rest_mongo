770343fd16fe3b9fa9607c0f16adf99b1b8007ed
diegogranados143@gmail.com <diego@codeando.edu.pe>
2024-04-08 19:57:35 -0400

    Get all books and create a book done

---
+++ src/models/book.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const bookSchema = new mongoose.Schema(
+    {
+        title: String,
+        author: String,
+        genre: String,
+        pub_date: String,
+    }
+)
+
+module.exports = mongoose.model('Book', bookSchema)
